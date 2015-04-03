06590c1550097c339be28cb228a1663fa7fc3c62
hassant <hassan.tariq2@ibexglobal.com>
2015-04-03 21:06:53 +0500

    commit

---
+++ Content/App/Controller.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    //Module
+    var sample = angular.module('popup', ['ngRoute']);
+
+    sample.directive("datepicker", function() {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function(scope, elem, attrs, ngModelCtrl) {
+                var updateModel = function(dateText) {
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(dateText);
+                    });
+                };
+                var options = {
+                    dateFormat: "mm/dd/yy",
+                    onSelect: function(dateText) {
+                        updateModel(dateText);
+                    }
+                };
+                elem.datepicker(options);
+            }
+        }
+    });
+
+    //Controller
+    sample.controller('popupController', function ($scope) {
+        $scope.showPopup = false;
+        $scope.errorMessage = false;
+
+        $scope.isPopupVisble = function () {
+            $scope.showPopup = true;
+        };
+        $scope.hidePrompt = function () {
+            $scope.EndDate = "";
+            $scope.errorMessage = false;
+            $scope.showPopup = false;
+        };
+        $scope.deleteRecord = function() {
+            var endDate = $scope.EndDate != null ? new Date($scope.EndDate) : "Invalid Date";
+            if (endDate == "Invalid Date") {
+                $scope.errorMessage = true;
+                return;
+            }
+            $scope.hidePrompt();
+        };
+    });
+
+})();
+
